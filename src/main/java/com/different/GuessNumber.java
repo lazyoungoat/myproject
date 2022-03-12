a512c6a880a88197967b6784d04e1624923f2f92
Polite <l.r.shiun@gmail.com>
2022-03-12 15:18:45 +0800

    double commit

--- src/main/java/com/different/GuessNumber.java
+++ src/main/java/com/different/GuessNumber.java
@@ -6,25 +6,29 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Random random = new Random();
-        int number = random.nextInt(10)+1;
+        int number = random.nextInt(100)+1;
         System.out.println(number);
 
         Scanner scanner = new Scanner(System.in);
 
         boolean test = false;
 
-        System.out.println("Please guess a number between 1 and 10.");
+        System.out.println("Please guess a number between 1 and 100.");
 
         for (int i = 0; i < 4; i++) {
             String string = scanner.next();
-            int input = Integer.parseInt(string);
+            double input = Double.parseDouble(string);
 
-            if (input != number){
-                System.out.println("You're wrong! Please guess again.");
 
+
+            if (input != number && (input < 1 || input > 100)){
+                System.out.println("Not in range!");
+
+            } else if (input != number && (1 <= input && input <= 100)){
+                System.out.println("You're wrong! Please try again.");
             }else {
                 test = true;
-            break;
+                break;
             }
 
 
