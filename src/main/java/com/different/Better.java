328690ce7478e42316ecd7f336d8ede5d8e433c1
Polite <l.r.shiun@gmail.com>
2022-03-13 17:16:59 +0800

    range commit

--- src/main/java/com/different/Better.java
+++ src/main/java/com/different/Better.java
@@ -2,6 +2,14 @@ package com.different;
 
 public class Better {
     public static void main(String[] args) {
+        double min = 1;
+        double input = 12;
 
+        System.out.println("old min: " + min +"\t"+ "old input: " + input);
+
+        double tmp = min;
+        min = input;
+        input = tmp;
+        System.out.println("new min: " + min +"\t"+ "new input: " + input);
     }
 }
--- src/main/java/com/different/GuessNumber.java
+++ src/main/java/com/different/GuessNumber.java
@@ -6,38 +6,51 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Random random = new Random();
-        int number = random.nextInt(100)+1;
+        int number = random.nextInt(100) + 1;
         System.out.println(number);
 
         Scanner scanner = new Scanner(System.in);
 
         boolean test = false;
 
-        System.out.println("Please guess a number between 1 and 100.");
+        double max = 100;
+        double min = 1;
+
+        System.out.println("Please guess a number between " + min + " and " + max);
 
         for (int i = 0; i < 4; i++) {
             String string = scanner.next();
             double input = Double.parseDouble(string);
 
-
-
-            if (input != number && (input < 1 || input > 100)){
+            if (input != number && (input < 1 || input > 100)) {
                 System.out.println("Not in range!");
 
-            } else if (input != number && (1 <= input && input <= 100)){
-                System.out.println("You're wrong! Please try again.");
-            }else {
+            } else if (input != number && (1 <= input && input <= 100)) {
+
+
+                if (input < number) {
+                    double tmp = min;
+                    min = input;
+                    System.out.println("You're wrong! Please try again. (" + min + " ~ " + max + ")");
+                } else if (input > number) {
+                    double tmp = max;
+                    max = input;
+
+                    System.out.println("You're wrong! Please try again. (" + min + " ~ " + max + ")");
+                }
+            } else {
                 test = true;
                 break;
             }
 
 
         }
-        if (test){
+        if (test) {
             System.out.println("Correct!!");
-        }else{
+        } else {
             System.out.println("No more chance!");
         }
-
     }
+
+
 }
